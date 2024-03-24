143826ea36b233a23fc4f6c16f58b09029bfcdca
Matsunaru <error1998pl@gmail.com>
2024-03-24 19:09:27 +0100

    doing stuff

--- Reder.cpp
+++ Reder.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+
 using namespace std;
 
+string filename;
+
 int main() {
-    cout << "Hello, World!" << endl;
+    cout << "Podaj nazwe pliku: " << endl;
+    cin >> filename >>endl;
+
+    ifstream plik(filename);
+    if (!plik.is_open())
+    {
+        cerr << "Nie udalo sie otworzyc pliku!" << endl;
+        return 1;
+    }
+    plik.close();
+
     return 0;
 }
